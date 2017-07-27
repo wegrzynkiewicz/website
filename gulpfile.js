7866fb21db312ccdd521d1c0069d89a4067518cb
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2017-07-27 22:41:06 +0200

    Finally

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var uncss = require('gulp-uncss');
-var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require('gulp-clean-css');
@@ -8,27 +7,31 @@ var selectors = require('gulp-selectors');
 var runSequence = require("run-sequence");
 var inlinesource = require('gulp-inline-source');
 
-var styles = [
-    'node_modules/bootstrap/dist/css/bootstrap-grid.min.css',
-    'src/styles/main.css'
-];
-
-gulp.task('build-scripts', function () {
+gulp.task('copy-resources', function () {
+    return gulp.src("resources/**/*")
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build-styles', function () {
-    return gulp.src(styles)
-        .pipe(concat('all.css'))
+    return gulp.src("src/**/*.css")
+        .pipe(concat('styles.css'))
         .pipe(uncss({
             html: ['src/index.html']
         }))
-        .pipe(cleanCSS())
+        .pipe(cleanCSS({
+            level: {
+                1: {
+                    specialComments: 'none'
+                }
+            }
+        }))
         .pipe(gulp.dest('build'));
 });
 
 gulp.task('build-html', function () {
     return gulp.src('src/index.html')
         .pipe(htmlmin({
+            minifyJS: true,
             collapseWhitespace: true,
             removeComments: true
         }))
@@ -36,11 +39,15 @@ gulp.task('build-html', function () {
 });
 
 gulp.task('minify-selectors', function () {
-    return gulp.src(['build/*.css', 'build/*.html', 'build/*.js'])
-        .pipe(selectors.run({
-            'css': ['css'],
-            'html': ['html']
-        }))
+    var processors = {
+        css: ['css'],
+        html: ['html']
+    };
+    var ignores = {
+        ids: '*'
+    };
+    return gulp.src(['build/**/*.css', 'build/*.html', 'build/*.js'])
+        .pipe(selectors.run(processors, ignores))
         .pipe(gulp.dest('build'));
 });
 
@@ -50,19 +57,13 @@ gulp.task('inject', function () {
             compress: false,
             rootpath: 'build'
         }))
-        .pipe(gulp.dest('build'));
-});
-
-gulp.task('clean', function () {
-    return gulp.src(['build/*', '!build/index.html'])
-        .pipe(clean());
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', function () {
     runSequence(
-        ['build-scripts', 'build-styles', 'build-html'],
+        ['copy-resources', 'build-styles', 'build-html'],
         'minify-selectors',
-        'inject',
-        'clean'
+        'inject'
     );
 });
